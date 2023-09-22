9d6c418ec6836920397ca30f69dcfbc6872265e7
br4tm <robert@meyer-leipzig.de>
2023-09-22 05:54:36 +0200

    Update datax-test.js

--- datax-test.js
+++ datax-test.js
@@ -1,27 +1,36 @@
-//document.addEventListener("DOMContentLoaded", function() {
-    // URL-Parameter auslesen
-    const urlParams = new URLSearchParams(window.location.search);
-    const dtid = urlParams.get("dtid");
-    
-    // Prüfen, ob dtid vorhanden ist
-    if (dtid) {
-        // POST-Request an deine REST-API senden
-        const apiUrl = "https://y8i6cu0dlct5i4c-rmlpz1dev.adb.eu-frankfurt-1.oraclecloudapps.com/ords/shpreporter/dataxtrackingservices/test";
-        fetch(apiUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ dtid }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log("API Response:", data);
-            // Hier kannst du je nach Bedarf weitere Aktionen durchführen
-        })
-        .catch(error => {
-            console.error("Error sending POST request:", error);
-        });
+getShopDomain() {
+    try {
+        if (Shopify) return Shopify.shop;
+        return ""
+    } catch (e) {
+        return console.error("no domain"), ""
     }
-//})
+}
+
+//document.addEventListener("DOMContentLoaded", function() {
+const urlParams = new URLSearchParams(window.location.search);
+const dtid = urlParams.get("dtid");
+const shopUrl = this.getShopDomain();
+
+if (dtid) {
+    const apiUrl = "https://y8i6cu0dlct5i4c-rmlpz1dev.adb.eu-frankfurt-1.oraclecloudapps.com/ords/shpreporter/dataxtrackingservices/test";
+
+    var content = {dtid, shopUrl};
+    
+    fetch(apiUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("API Response:", data);
+    })
+    .catch(error => {
+        console.error("Error sending POST request:", error);
+    });
+}
 console.log('DataX loaded')
+
