aa42b3fba9de9a7df74d993200372aa5081387a8
br4tm <robert@meyer-leipzig.de>
2023-09-25 12:37:54 +0200

    Update datax-test.js

--- datax-test.js
+++ datax-test.js
@@ -1,36 +1,77 @@
-function getShopDomain() {
-    try {
-        if (Shopify) return Shopify.shop;
-        return ""
-    } catch (e) {
-        return console.error("no domain"), ""
+class DataX {
+    constructor() {
+        this.apiUrl = "https://y8i6cu0dlct5i4c-rmlpz1dev.adb.eu-frankfurt-1.oraclecloudapps.com/ords/shpreporter/dataxtrackingservices/test";
+        this.dtid = this.getDtidFromUrl();
+        this.shopUrl = this.getShopDomain();
+
+        if (this.dtid) {
+            document.addEventListener("DOMContentLoaded", () => {
+                if (!this.getCookie(`sent_dtid_${this.dtid}`)) {
+                    this.sendDtid();
+                }
+            });
+        }
+    }
+
+    getShopDomain() {
+        try {
+            if (Shopify) return Shopify.shop;
+            return "";
+        } catch (e) {
+            console.error("no domain");
+            return "";
+        }
+    }
+
+    getDtidFromUrl() {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get("dtid");
+    }
+
+    setCookie(name, value, days) {
+        let expires = "";
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+
+    getCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    sendDtid() {
+        const content = {
+            dtid: this.dtid,
+            shopUrl: this.shopUrl
+        };
+        
+        fetch(this.apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ content }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("API Response:", data);
+            this.setCookie(`sent_dtid_${this.dtid}`, true, 365);  // Mark this dtid as sent for 1 year
+        })
+        .catch(error => {
+            console.error("Error sending POST request:", error);
+        });
     }
 }
 
-//document.addEventListener("DOMContentLoaded", function() {
-const urlParams = new URLSearchParams(window.location.search);
-const dtid = urlParams.get("dtid");
-const shopUrl = this.getShopDomain();
-
-if (dtid) {
-    const apiUrl = "https://y8i6cu0dlct5i4c-rmlpz1dev.adb.eu-frankfurt-1.oraclecloudapps.com/ords/shpreporter/dataxtrackingservices/test";
-
-    var content = {dtid, shopUrl};
-    
-    fetch(apiUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ content }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("API Response:", data);
-    })
-    .catch(error => {
-        console.error("Error sending POST request:", error);
-    });
-}
-console.log('DataX loaded')
-
+const dataXInstance = new DataX();
+console.log('DataX loaded');
